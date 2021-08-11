abe81193b625744d22d9403da733e294efb19a55
le7els10 <le7els10@gmail.com>
2021-08-11 18:37:24 -0500

    heroes app

---
+++ src/app/heroes/heroes-routing.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './pages/add/add.component';
+import { ListComponent } from './pages/list/list.component';
+import { SearchComponent } from './pages/search/search.component';
+import { HeroComponent } from './pages/hero/hero.component';
+import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
+        path: 'listado',
+        component: ListComponent,
+      },
+      {
+        path: 'agregar',
+        component: AddComponent,
+      },
+      {
+        path: 'editar/:id',
+        component: AddComponent,
+      },
+      {
+        path: 'buscar',
+        component: SearchComponent,
+      },
+      {
+        path: ':id',
+        component: HeroComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'listado',
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class HeroesRoutingModule {}
